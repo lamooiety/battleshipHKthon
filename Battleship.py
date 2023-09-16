3799fe007101a16a7bf682c0eb2d15a7cf994128
lamooiety <clairesss.no1@gmail.com>
2023-09-16 15:44:40 +1000

    upload for debugging

--- Battleship.py
+++ Battleship.py
@@ -58,6 +58,16 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
         
         coord = random.choice(crossboard)
         crossboard.remove(coord)
+
+        # make sure it is not hit before
+        while (coord in p1PrevHit and crossboard != []):
+            coord = random.choice(crossboard)
+            crossboard.remove(coord)
+
+        # return to hunt
+        if crossboard == []:
+            storage = []
+
         storage.append(crossboard)
 
         # store the success coordinate
@@ -69,6 +79,8 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
     
     # test the crossboard until hit again
     elif storage[0] == 2:
+
+        # hit the crossboard
         if p1PrevHit:
             # move to step 3
             storage[0] == 3
@@ -83,21 +95,69 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
             # add success coordinate
             storage[2].append(prevCoord)
 
+            # back to hunting if it meet the length
+            if storage[2].length >= 5:
+                return p1ShotSeq[-1], []
+
+
             # check it's following the columns or rows
+            # following columns
             if prevX == prevprevX:
-                # following rows
-                for 
-
+                min = 10
+                max = 1
+                for cod in storage[2]:
+                    if cod[1] < min:
+                        min = cod[1]
+                    if cod[1] > max:
+                        max = cod[1]
+                
+                # check if the both sides are hit or not
+                if min == 1 or [prevX, min-1] in p1ShotSeq:
+                    if max == 10 or [prevX, max+1] in p1ShotSeq:
+                        # back to hunting
+                        storage = []
+                        return [prevX, max+1], storage
+                    else:
+                        return [prevX, max+1], storage
+                else:
+                    return [prevX, min-1], storage
+                
+            # followng rows
             else:
-                # followng columns
-            
+                min = 10
+                max = 1
+                for cod in storage[2]:
+                    if cod[0] < min:
+                        min = cod[0]
+                    if cod[0] > max:
+                        max = cod[0]
+                
+                # check if the both sides are hit or not
+                if min == 1 or [min-1, prevY] in p1ShotSeq:
+                    if max == 10 or [max+1, prevY] in p1ShotSeq:
+                        # back to hunting
+                        return [max+1, prevY], []
+                    else:
+                        return [max+1, prevY], storage
+                else:
+                    return [min-1, prevY], storage
 
 
-
+        # does not hit the crossboard
         else:
             crossboard = storage[1]
             coord = random.choice(crossboard)
             crossboard.remove(coord)
+
+            # make sure it is not hit before
+            while (coord in p1ShotSeq):
+                coord = random.choice(crossboard)
+                crossboard.remove(coord)
+            
+            # return to hunt
+            if crossboard == []:
+                storage = []
+
             storage[1] = crossboard
 
             return coord, storage
@@ -105,13 +165,68 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
 
     # continue hit until both sides are empty
     elif storage[0] == 3:
-    # check crossboard (step 3)
-    # elif storage[0] == 2:
-    #     if p1PrevHit:
+
+        # hit the side
+        prevCoord = p1ShotSeq[-1]
+        prevX = prevCoord[0]
+        prevY = prevCoord[1]
+        prevprevCoord = p1ShotSeq[-2]
+        prevprevX = prevprevCoord[0]
+        prevprevY = prevprevCoord[1]
+
+        # add success coordinate
+        if p1PrevHit:
+            storage[2].append(prevCoord)
+
+        # back to hunting if it meet the length
+        if storage[2].length >= 5:
+            return p1ShotSeq[-1], []
 
 
+        # check it's following the columns or rows
+        # following columns
+        if prevX == prevprevX:
+            min = 10
+            max = 1
+            for cod in storage[2]:
+                if cod[1] < min:
+                    min = cod[1]
+                if cod[1] > max:
+                    max = cod[1]
+            
+            # check if the both sides are hit or not
+            if min == 1 or [prevX, min-1] in p1ShotSeq:
+                if max == 10 or [prevX, max+1] in p1ShotSeq:
+                    # back to hunting
+                    storage = []
+                    return [prevX, max+1], storage
+                else:
+                    return [prevX, max+1], storage
+            else:
+                return [prevX, min-1], storage
+            
+        # followng rows
+        else:
+            min = 10
+            max = 1
+            for cod in storage[2]:
+                if cod[0] < min:
+                    min = cod[0]
+                if cod[0] > max:
+                    max = cod[0]
+            
+            # check if the both sides are hit or not
+            if min == 1 or [min-1, prevY] in p1ShotSeq:
+                if max == 10 or [max+1, prevY] in p1ShotSeq:
+                    # back to hunting
+                    storage = []
+                    return [max+1, prevY], storage
+                else:
+                    return [max+1, prevY], storage
+            else:
+                return [min-1, prevY], storage
 
     else:
         x = random.randint(1,10)
         y = random.randint(1,10)
-        return [x,y], storage
+        return [x,y], []
