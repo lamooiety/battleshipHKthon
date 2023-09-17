687dfc59e114b3a7f03edcc1c4da523c22304f2a
lamooiety <clairesss.no1@gmail.com>
2023-09-17 11:13:54 +1000

    Revert "add heat map"
    This reverts commit 9dcd1b031cf85e29050bd3cf6b6436f12e7d8b4a.

--- Battleship.py
+++ Battleship.py
@@ -1,67 +1,46 @@
 import random
 GRIDSIZE = 10
 
+# storage == [] or storage[0] == 1 : hunting
+# storage[0] == 2: cross search
+
 def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
     
 
     # hunting for the battelship
     if not p1PrevHit and (storage == [] or storage[0] == 1):
-
-        # start
         if storage == []:
             storage.append(1)
-            # create a heat map
-            heat_map = {(x, y): random.random() for x in range(GRIDSIZE) for y in range(GRIDSIZE)}
-    
-            # Assign values to heat map based on previous shots (higher values for unhit areas).
-            for x, y in p1ShotSeq:
-                if (x, y) in heat_map:
-                    heat_map[(x, y)] += 2
-            
-            # Assign values to heat map based on remaining HP (higher values for more HP).
-            for x in range(GRIDSIZE):
-                for y in range(GRIDSIZE):
-                    if (x, y) in heat_map:
-                        heat_map[(x, y)] += yourHp + enemyHp
-            storage.append(heat_map)
-
 
         # get available coordinates
-        # blackboard = []
-        # whiteboard = []
-        # for i in range(1, GRIDSIZE+1):
-        #     for j in range(1, GRIDSIZE+1):
-        #         if (i + j) % 2 == 0:
-        #             blackboard.append([i,j]) 
-        #         else:
-        #             whiteboard.append([i,j])
+        blackboard = []
+        whiteboard = []
+        for i in range(1, GRIDSIZE+1):
+            for j in range(1, GRIDSIZE+1):
+                if (i + j) % 2 == 0:
+                    blackboard.append([i,j]) 
+                else:
+                    whiteboard.append([i,j])
         
-        # for prev in p1ShotSeq:
-        #     if prev in blackboard:
-        #         blackboard.remove(prev)
-        #     else:
-        #         whiteboard.remove(prev)
+        for prev in p1ShotSeq:
+            if prev in blackboard:
+                blackboard.remove(prev)
+            else:
+                whiteboard.remove(prev)
 
-        # # if the blackboard is empty, we have to chooce from the whiteboard
-        # if blackboard != []:
-        #     coord = random.choice(blackboard)
-        # else:
-        #     coord = random.choice(whiteboard)
-
-        heat_map = storage[1]
-        sorted_coordinates = sorted(heat_map.keys(), key=lambda coord: heat_map[coord], reverse=True)
-    
-        # Find the best coordinate that hasn't been targeted before.
-        for coord in sorted_coordinates:
-            if coord not in p1PrevHit:
-                return coord
+        # if the blackboard is empty, we have to chooce from the whiteboard
+        if blackboard != []:
+            coord = random.choice(blackboard)
+        else:
+            coord = random.choice(whiteboard)
 
         return coord, storage
     
     # first hit an object
     elif p1PrevHit and (storage == [] or storage[0] == 1):
         # update the storage to step 2
-        storage = [2, storage[1]]
+        storage = []
+        storage.append(2)
 
         # get the previous coordinate
         prevCoord = p1ShotSeq[-1]
@@ -87,7 +66,7 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
 
         # return to hunt
         if crossboard == []:
-            return coord, [0, storage[1]]
+            return coord, []
 
         storage.append(crossboard)
 
@@ -114,11 +93,11 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
             prevprevY = prevprevCoord[1]
 
             # add success coordinate
-            storage[3].append(prevCoord)
+            storage[2].append(prevCoord)
 
             # back to hunting if it meet the length
-            if len(storage[3]) >= 5:
-                return p1ShotSeq[-1], [0, storage[1]]
+            if len(storage[2]) >= 5:
+                return p1ShotSeq[-1], []
 
 
             # check it's following the columns or rows
@@ -126,7 +105,7 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
             if prevX == prevprevX:
                 min = 10
                 max = 1
-                for cod in storage[3]:
+                for cod in storage[2]:
                     if cod[1] < min:
                         min = cod[1]
                     if cod[1] > max:
@@ -136,7 +115,7 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
                 if min == 1 or [prevX, min-1] in p1ShotSeq:
                     if max == 10 or [prevX, max+1] in p1ShotSeq:
                         # back to hunting
-                        storage = [0, storage[1]]
+                        storage = []
                         return [prevX, max+1], storage
                     else:
                         return [prevX, max+1], storage
@@ -147,7 +126,7 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
             else:
                 min = 10
                 max = 1
-                for cod in storage[3]:
+                for cod in storage[2]:
                     if cod[0] < min:
                         min = cod[0]
                     if cod[0] > max:
@@ -157,7 +136,7 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
                 if min == 1 or [min-1, prevY] in p1ShotSeq:
                     if max == 10 or [max+1, prevY] in p1ShotSeq:
                         # back to hunting
-                        return [max+1, prevY], [0, storage[1]]
+                        return [max+1, prevY], []
                     else:
                         return [max+1, prevY], storage
                 else:
@@ -166,7 +145,7 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
 
         # does not hit the crossboard
         else:
-            crossboard = storage[2]
+            crossboard = storage[1]
             coord = random.choice(crossboard)
             crossboard.remove(coord)
 
@@ -177,9 +156,9 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
             
             # return to hunt
             if crossboard == []:
-                return coord, [0, storage[1]]
+                return coord, []
 
-            storage[2] = crossboard
+            storage[1] = crossboard
 
             return coord, storage
 
@@ -197,11 +176,11 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
 
         # add success coordinate
         if p1PrevHit:
-            storage[3].append(prevCoord)
+            storage[2].append(prevCoord)
 
         # back to hunting if it meet the length
-        if len(storage[3]) >= 5:
-            return p1ShotSeq[-1], [0, storage[1]]
+        if len(storage[2]) >= 5:
+            return p1ShotSeq[-1], []
 
 
         # check it's following the columns or rows
@@ -209,7 +188,7 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
         if prevX == prevprevX:
             min = 10
             max = 1
-            for cod in storage[3]:
+            for cod in storage[2]:
                 if cod[1] < min:
                     min = cod[1]
                 if cod[1] > max:
@@ -219,7 +198,7 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
             if min == 1 or [prevX, min-1] in p1ShotSeq:
                 if max == 10 or [prevX, max+1] in p1ShotSeq:
                     # back to hunting
-                    storage = [0, storage[1]]
+                    storage = []
                     return [prevX, max+1], storage
                 else:
                     return [prevX, max+1], storage
@@ -230,7 +209,7 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
         else:
             min = 10
             max = 1
-            for cod in storage[3]:
+            for cod in storage[2]:
                 if cod[0] < min:
                     min = cod[0]
                 if cod[0] > max:
@@ -240,7 +219,7 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
             if min == 1 or [min-1, prevY] in p1ShotSeq:
                 if max == 10 or [max+1, prevY] in p1ShotSeq:
                     # back to hunting
-                    storage = [0, storage[1]]
+                    storage = []
                     return [max+1, prevY], storage
                 else:
                     return [max+1, prevY], storage
@@ -250,4 +229,4 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
     else:
         x = random.randint(1,10)
         y = random.randint(1,10)
-        return [x,y], [0, storage[1]]
+        return [x,y], []
